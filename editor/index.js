38a18f3d74ef49ab4ca4bd4ec681bd6c4dd993c0
Nathaniel Knight <nathaniel.ep@gmail.com>
2022-10-27 11:38:28 -0700

    Add CodeMirror

---
+++ editor/index.js
@@ -0,0 +1,24 @@
+import { basicSetup, EditorView } from "codemirror"
+import { markdown } from "@codemirror/lang-markdown"
+
+// Hide the existing body
+let textarea = document.querySelector('textarea');
+textarea.style.display = "none";
+
+// Create a CodeMirror editor with the body's contents
+let view = new EditorView({
+    doc: textarea.value,
+    extensions: [
+        basicSetup,
+        markdown({})
+    ],
+});
+textarea.insertAdjacentElement("afterend", view.dom);
+
+
+// When submitting the form, update the original body with the new contents
+textarea.parentElement.onsubmit = function () {
+    textarea.value = view.state.doc;
+}
+
+
